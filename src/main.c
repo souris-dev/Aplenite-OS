76017b33c2003ec438c8084a13d9cfad62d6981a
Souris Ash <suorees113@gmail.com>
2013-09-03 22:07:26 +0530

    Added GDT, IDT, and ISRs

--- src/main.c
+++ src/main.c
@@ -3,13 +3,22 @@
 /////////////////////////////////////////
 
 #include "monitor.h"
+#include "descriptor_tables.h"
 
 int main(struct multiboot *mboot_ptr)
-{
+{   
+    // Initialise all the ISRs and segmentation
+    init_descriptor_tables();
+
     // Initialise the screen (by clearing it)
     monitor_clear();
     // Write out a sample string
-    monitor_write("Hello, world!");
+    monitor_write("Welcome to Aplenite OS!\n");
+    monitor_write("\nType in 'help' for a list of commands, and 'about'\nfor version info.\n\n");
     
+    // execute some asm statements (instructiosns)
+    asm volatile("int $0x3");
+    asm volatile("int $0x4");
+
     return 0;
 }
